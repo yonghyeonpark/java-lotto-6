8714574507eeff34d53dbd41fb819707548bf2bd
YongHyeon <pyh4616@naver.com>
2023-11-29 22:22:09 +0900

    refactor(Rank): 변수에 일치하는 개수를 추가, 모든 변수에 대해 getter 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -2,17 +2,31 @@ package lotto.domain;
 
 public enum Rank {
 
-    FIRST_PLACE("6개 일치", 2_000_000_000),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치", 30_000_000),
-    THIRD_PLACE("5개 일치", 1_500_000),
-    FOURTH_PLACE("4개 일치", 50_000),
-    FIFTH_PLACE("3개 일치", 5_000);
+    FIRST_PLACE(6, "6개 일치", 2_000_000_000),
+    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치", 30_000_000),
+    THIRD_PLACE(5, "5개 일치", 1_500_000),
+    FOURTH_PLACE(4, "4개 일치", 50_000),
+    FIFTH_PLACE(3, "3개 일치", 5_000);
 
+    private int matchingCount;
     private String detail;
     private int prizeMoney;
 
-    Rank(String detail, int prizeMoney) {
+    Rank(int matchingCount, String detail, int prizeMoney) {
+        this.matchingCount = matchingCount;
         this.detail = detail;
         this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchingCount() {
+        return matchingCount;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 }
