8e37fddd50797fb5ef4be4a2621cf6e3ace29065
YongHyeon <pyh4616@naver.com>
2023-11-29 22:27:15 +0900

    feat(WinningLotto): 클래스(WinningResult)를 변수로 추가하고, 당첨 번호와 티켓을 비교하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -8,16 +8,32 @@ public class WinningLotto {
 
     private final List<Integer> numbers;
     private final int bonusNumber;
+    private final WinningResult winningResult;
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         validateDuplicate(numbers, bonusNumber);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
+        this.winningResult = new WinningResult();
     }
 
     private void validateDuplicate(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(LOTTO_AND_BONUS_DUPLICATE_ERROR_MESSAGE);
         }
+    }
+
+    public void compareWithTicket(List<Integer> ticket) {
+        int matchingCount = 0;
+        boolean hasMatchingBonusNumber = false;
+        for (int number : ticket) {
+            if (numbers.contains(number)) {
+                matchingCount++;
+            }
+            if (bonusNumber == number) {
+                hasMatchingBonusNumber = true;
+            }
+        }
+        winningResult.judgeRank(matchingCount, hasMatchingBonusNumber);
     }
 }
