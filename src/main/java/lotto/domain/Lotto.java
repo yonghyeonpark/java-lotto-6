af94d645a7e8885200c1f5332c486ef2a36f73f4
YongHyeon <pyh4616@naver.com>
2023-11-29 20:15:25 +0900

    feat(Lotto): 로또 번호에 대한 유효성 검사 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,41 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+
+    private static final String LOTTO_NUMBER_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개입니다.";
+    private static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_NUMBER_SIZE_ERROR_MESSAGE);
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number > 45 || number < 1) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        long deduplication = numbers.stream().distinct().count();
+        if (deduplication != numbers.size()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
 }
