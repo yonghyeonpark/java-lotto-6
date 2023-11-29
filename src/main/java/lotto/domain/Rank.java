b828696b28f1b0569c202d86da13e65ab0f21d88
YongHyeon <pyh4616@naver.com>
2023-11-29 22:59:05 +0900

    refactor(Rank): 반복문에 사용하기 위해 열거의 순서 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -2,11 +2,11 @@ package lotto.domain;
 
 public enum Rank {
 
-    FIRST_PLACE(6, "6개 일치", 2_000_000_000),
-    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치", 30_000_000),
-    THIRD_PLACE(5, "5개 일치", 1_500_000),
+    FIFTH_PLACE(3, "3개 일치", 5_000),
     FOURTH_PLACE(4, "4개 일치", 50_000),
-    FIFTH_PLACE(3, "3개 일치", 5_000);
+    THIRD_PLACE(5, "5개 일치", 1_500_000),
+    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치", 30_000_000),
+    FIRST_PLACE(6, "6개 일치", 2_000_000_000);
 
     private int matchingCount;
     private String detail;
