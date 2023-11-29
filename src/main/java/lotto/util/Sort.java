bb9742c3fc41fa0916e7c7a52ed3f21a9dd1c6ae
YongHyeon <pyh4616@naver.com>
2023-11-29 19:50:04 +0900

    feat(Sort): 리스트를 오름차순 정렬하는 기능 구현

---
+++ src/main/java/lotto/util/Sort.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Sort {
+
+    public static void naturalOrder(List<Integer> list) {
+        Collections.sort(list);
+    }
+}
