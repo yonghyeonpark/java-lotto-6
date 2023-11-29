6f842744a9c12ca5cd32fd42ad1e3442f260d377
YongHyeon <pyh4616@naver.com>
2023-11-29 19:50:41 +0900

    feat(Convert): 문자열을 정수형 리스트로 변환하는 기능 구현

--- src/main/java/lotto/util/Convert.java
+++ src/main/java/lotto/util/Convert.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Convert {
 
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력하셔야 합니다.";
@@ -10,5 +13,15 @@ public class Convert {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
         }
+    }
+
+    public static List<Integer> stringToIntegerListByNaturalOrder(String input) {
+        List<Integer> list = new ArrayList<>();
+        String[] split = input.split(",");
+        for (String element : split) {
+            list.add(stringToInt(element));
+        }
+        Sort.naturalOrder(list);
+        return list;
     }
 }
